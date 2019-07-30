71d58ae2c069311a8dd1399074618a0dbbcfb681
Jam Nguyen <jam@jamnguyen.com>
2019-07-30 18:37:31 +0700

    Update port.

--- index.js
+++ index.js
@@ -214,6 +214,7 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log('Listening on port 3000...');
+const port = process.env.PORT || 3000;
+http.listen(port, () => {
+  console.log('Listening on port ' + port + '...');
 });
